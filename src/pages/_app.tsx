6c532873039668216bb9fc6c0e3f4cde7a99ebc3
Alex Harvey <alex@contic.co.uk>
2023-04-29 18:31:40 +0100

    Improve accessibility and SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import { Lato } from '@next/font/google';
 import '../../styles/globals.css';
+import Head from 'next/head';
 
 const lato = Lato({
   weight: ['100', '300', '400', '700', '900'],
@@ -11,10 +12,16 @@ const lato = Lato({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class">
-      <main className={`${lato.className} scroll-smooth`}>
-        <Component {...pageProps} />
-      </main>
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>alexharvey.dev</title>
+        <meta name="description" content="Alex Harvey's personal dev site" />
+      </Head>
+      <ThemeProvider attribute="class">
+        <main className={`${lato.className} scroll-smooth`}>
+          <Component {...pageProps} />
+        </main>
+      </ThemeProvider>
+    </>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html style={{ scrollBehavior: 'smooth' }} className="dark">
+    <Html style={{ scrollBehavior: 'smooth' }} className="dark" lang="en-GB">
       <Head />
       <body>
         <Main />
